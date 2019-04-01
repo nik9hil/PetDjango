98a3c02d75e4bf1a258665e043553698a8699e10
Darshan Chheda <ddc@somaiya.edu>
2019-04-01 20:11:16 +0530

    Fixed Models.py

--- Pet2Vet/models.py
+++ Pet2Vet/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Volunteer(models.Model):
-    yourname = models.CharField(max_length=100)
+    yourname = models.CharField(max_length=100,blank=True)
     email = models.EmailField(max_length=150,blank=True)
-    phonenumber = models.CharField(max_length=10)
-    city = models.CharField(max_length=50)
+    phonenumber = models.CharField(max_length=10,blank=True)
+    city = models.CharField(max_length=50,blank=True)
