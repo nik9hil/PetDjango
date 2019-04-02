a41a0da1a5bade0da481089503e411a787409e04
Darshan Chheda <ddc@somaiya.edu>
2019-04-02 09:57:26 +0530

    Basic Structure created for models

--- Pet2Vet/admin.py
+++ Pet2Vet/admin.py
@@ -1,6 +1,10 @@
 #Import Dependency
 from django.contrib import admin
-from .models import Volunteer
+from .models import Volunteer,Vet,DonateForm,FirstAid,AdoptForm
 
 # Register your models here.
 admin.site.register(Volunteer)
+admin.site.register(Vet)
+admin.site.register(DonateForm)
+admin.site.register(FirstAid)
+admin.site.register(AdoptForm)
--- Pet2Vet/models.py
+++ Pet2Vet/models.py
@@ -7,3 +7,42 @@ class Volunteer(models.Model):
     email = models.EmailField(max_length=150,blank=True)
     phonenumber = models.CharField(max_length=10,blank=True)
     city = models.CharField(max_length=50,blank=True)
+
+    def __str__(self):
+        return self.yourname
+
+
+class Vet(models.Model):
+    name = models.CharField(max_length=100,blank=False)
+    city = models.CharField(max_length=50,blank=False)
+    email = models.EmailField(max_length=100,blank=False)
+    phonenumber = models.CharField(max_length=10,blank=False)
+    fees = models.CharField(max_length=100,blank=True)
+
+    def __str__(self):
+        return self.name
+
+class DonateForm(models.Model):
+    name = models.CharField(max_length=100,blank=False)
+    city = models.CharField(max_length=100,blank=False)
+    phonenumber = models.CharField(max_length=10,blank=False)
+
+    def __str__(self):
+        return self.name
+
+class FirstAid(models.Model):
+    title = models.CharField(max_length=100,blank=False)
+    video = models.CharField(max_length=100,blank=False)
+    
+    def __str__(self):
+        return self.title
+
+class AdoptForm(models.Model):
+    name = models.CharField(max_length=100,blank=False)
+    phonenumber = models.CharField(max_length=10,blank=False)
+    email = models.CharField(max_length=100,blank=False)
+    city = models.CharField(max_length=100,blank=False)
+    animal = models.CharField(max_length=100,blank=False)
+
+    def __str__(self):
+        return self.name
